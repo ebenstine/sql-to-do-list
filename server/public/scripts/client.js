dba4f63769c42a9624054f33aa566b6b0c077144
Eben Stine <ebenstine@gmail.com>
2021-08-06 19:12:24 -0500

    finished router code

---
+++ server/public/scripts/client.js
@@ -0,0 +1,100 @@
+console.log('js');
+
+$(document).ready(function () {
+    console.log('JQ');
+    // Establish Click Listeners
+    setupClickListeners()
+    // load existing koalas on page load
+    refreshTasks();
+    handleSubmit();
+}); // end doc ready
+
+function setupClickListeners() {
+    $('#taskList').on('click', '.deleteBtn', deleteTask);
+    $('#submitBtn').on('click', function () {
+
+        let taskToDo = {
+            description: $('#taskInput').val(),
+            priority: $('#priorityInput').val()
+        };
+        saveTask(taskToDo);
+    });
+}
+
+function getTasks(){
+    console.log( 'in getTasks');
+    $.ajax({
+        type: 'GET',
+        url: '/tasks'
+    }).then(function(response) {
+        console.log(response);
+        renderTasks(response);
+    }).catch(function(error){
+        console.log('error in GET!', error);
+    });
+}//end getTasks
+
+function saveTask(newTask){
+    console.log('in saveTask', newTask);
+    $.ajax({
+        type: 'POST',
+        url: '/tasks',
+        data: newTask
+    }).then((response) =>{
+        console.log(response);
+        $('#taskInput').val('')
+        $('#priorityInput').val('')
+        getTasks();
+    }).catch ((error) => {
+        console.log('error in POST!', error);
+        alert('Unable to add task!');
+
+    });
+}
+
+function deleteTask(){
+    console.log('in deleteTask', $(this));
+
+    const taskId = $(this).parents('tr').data('id')
+    console.log(taskId);
+    $.ajax ({
+        type: 'DELETE',
+        url: `/tasks/${taskId}`,
+     }).then((response) => {
+         console.log(response);
+         getTasks();
+     }).catch((error) => {
+         console.log('error in DELETE!', error);
+         alert('Unable to delete task!');
+     })
+}
+
+function markTaskComplete(){
+    let taskId = $(this).closest('tr').data().id;
+    console.log('task id', taskId);
+    $.ajax({
+        type: 'PUT',
+        url: `/tasks/${taskId}`
+    }).then(function(response){
+        getTasks(response);
+    }).catch(function(error){
+        console.log('error in PUT!', error);
+    })
+}
+
+
+function renderTasks(tasks){
+    let outputElement = $('#taskList');
+    outputElement.empty();
+    for (const task of tasks) {
+        outputElement.append(`
+            <tr data-id="${task.id}">
+                <th>${task.description}</th>
+                <th>${task.priority}</th>
+                <th><button class="toggleComplete">${task.complete ? "Complete": "Incomplete"}</button><th>
+                <th><button class-"deleteBtn"> Delete </button>
+            </tr>
+            `);
+    }
+
+} 
--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -5,17 +5,86 @@ const tasksRouter = express.Router();
 const pool = require('../modules/pool');
 
 //GET 
+tasksRouter.get('/', (req, res) => {
+    let queryText = `
+    SELECT "id", "description", "priority"
+    FROM "tasks"
+    ORDER BY "priority";
+    `;
+    pool.query(queryText).then(result => {
+        res.send(result.rows);
+    }).catch(error => {
+        console.log('error getting tasks', error);
+        res.sendStatus(500);
+    });
 
+});
 
+//POST
+tasksRouter.post('/', (req, res) => {
+    let newTask = req.body;
+    console.log('new task:', newTask);
 
+    let queryText = `
+        INSERT INTO "tasks"
+            ("description, "priority")
+        VALUES
+            ($1, $2);
+    
+    `;
+    let sqlParams = [
+        newTask.description,
+        newTask.priority
+    ];
+    console.log(sqlParams);
 
+    pool.query(queryText, sqlParams)
+        .then((dbRes) => {
+            res.sendStatus(201);
+        }).catch((error) => {
+            console.log('error in POST!', error);
+            res.sendStatus(500);
+        });
+});
+//PUT
+tasksRouter.put('/:id', (req, res) => {
+    let id = req.params.id
+    console.log(`updating task ${id}`);
 
+    let queryText = `
+        UPDATE "tasks"
+        SET "complete" = NOT "complete"
+        WHERE "id" = $1;
+    `;
 
+    params = [ id ];
+    pool.query(queryText, params)
+        .then((dbRes) => {
+            res.sendStatus(201);
+        }).catch((error) => {
+            console.log('error in PUT!', error);
+            res.sendStatus(500);
+        });
+})
 
-
-
-
-
-
+//DELETE
+tasksRouter.delete('/:id', (req, res) => {
+    let queryText = `
+        DELETE FROM "tasks"
+        WHERE "id" = $1
+    
+    `;
+    let sqlParams = [
+        req.params.id
+    ];
+    console.log(sqlParams);
+    pool.query(queryText, sqlParams)
+        .then((dbRes) => {
+            res.sendStatus(200);
+        }).catch((error) => {
+            console.log('error in DELETE!', error);
+            res.sendStatus(500);
+        });
+})
 
 module.exports = tasksRouter
